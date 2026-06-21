7e96e74983d7c02713760b3ced5d6df326cf4044
Volodymyr Yusyp <volodymyr.yusyp@gmail.com>
2026-06-21 23:38:31 +0300

    nothing to say

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app.add_middleware(
         'http://localhost:5173',
         'http://localhost:4173',
         'http://127.0.0.1:5173',
-        'https://first-self-propelled.vercel.app/'
+        'https://first-self-propelled.vercel.app'
     ],
     allow_credentials=True,
     allow_methods=['*'],

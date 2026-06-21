132c025b8c4f18970e5132d162d48ba7b16feb10
Volodymyr Yusyp <volodymyr.yusyp@gmail.com>
2026-06-21 23:30:09 +0300

    add cors

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,8 @@ app.add_middleware(
     allow_origins=[ # Сюди свій продакшн домен коли деплоїм
         'http://localhost:5173',
         'http://localhost:4173',
-        'http://127.0.0.1:5173'
+        'http://127.0.0.1:5173',
+        'https://first-self-propelled.vercel.app/'
     ],
     allow_credentials=True,
     allow_methods=['*'],
